5492e28c7129c69941d9b69ef4463ef6b23c2539
Cristian D. Serra <105523175+KristopherSerra@users.noreply.github.com>
2023-08-01 11:20:42 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/Facil")
+def facil():
+    return render_template("Facil.html")
+
+@app.route("/Medio")
+def medio():
+    return render_template("Medio.html")
+
+@app.route("/Dificil")
+def dificil():
+    return render_template("Dificil.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=2023)
